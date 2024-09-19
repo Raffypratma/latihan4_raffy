e28393ac6e84afc7712a1b0df7b4ab76176d601b
Raffypratma git <raffymarhas@gmail.com>
2024-09-19 09:31:48 +0700

    pushkedua

---
+++ lib/page2.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class page2 extends StatelessWidget {
+  const page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child:Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+          Row(
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.green,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.blue,
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.amber,
+              )
+            ],
+          )
+          ],
+        )
+      ),
+    );
+  }
+}
